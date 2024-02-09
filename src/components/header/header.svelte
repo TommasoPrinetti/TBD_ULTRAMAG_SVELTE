c79d1ea7e299d60276021e49bf1d369a1e35f1aa
TommasoPrinetti <Tommi.prinetti@gmail.com>
2024-02-09 16:37:32 +0100

    TBD LOGO

--- src/components/header/header.svelte
+++ src/components/header/header.svelte
@@ -1,6 +1,6 @@
 <script>
 
-  import TbdLogo from 'src/lib/webresources/IDENTITY_IMAGES/ABOUT_ultra.png';
+  import TbdLogo from '$webresources/IDENTITY_IMAGES/tbd_LOGO.webp';
   export let headerVar = 'COMMON';
 
   const logoHref = '/';
