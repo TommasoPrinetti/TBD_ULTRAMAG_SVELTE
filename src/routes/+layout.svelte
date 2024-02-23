ea49c1546e09d37e1c465ee7c4e205c88d56be5d
TommasoPrinetti <Tommi.prinetti@gmail.com>
2024-02-23 13:30:04 +0100

    Vercel analytics deployed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
     import '$lib/styles/style.css';
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+
+    inject({ mode: dev ? 'development' : 'production' });
+
 
     import articlesData from '$lib/articles.json';
 
